ea4f78e5a22740f0f174950314b93717688be05d
Prashant Bhattarai <prashantbhattarai696@gmail.com>
2020-12-30 19:02:12 +0545

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h2 className="section-title">about us</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum quam a illo quas. Ad, laudantium sint. Nobis distinctio quae sit labore odit iusto id laborum, vero rem voluptatum ad alias cum blanditiis, praesentium sapiente tempora eveniet quis. Beatae porro adipisci expedita doloremque. Dolores, nobis?</p>
+    </section>
+  )
+}
+
+export default About
